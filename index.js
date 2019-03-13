a4be674c5dc1302effed01ed7c209eb6be88d7b8
Zachary Lehmann <zlehmann@gmail.com>
2019-03-13 20:04:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
-    console.log('Welcome ${names[i]}! You are employee #${i + 1}');
+    console.log('Welcome ${names[i]}! You are employee #${i + 1}.');
   }
   return names;
 }
