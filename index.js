ed845e553cce82ba3ff7c7a630b92f8abad10323
Zachary Lehmann <zlehmann@gmail.com>
2019-03-13 20:27:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log('Welcome ' + names[i] + '!' + ' You are employee #' + (i + 1)+ '.');
+  }
+  return names;
+}
+
+function coinFlip() {
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+  count = 0;
+
+  while (coinFlip()) {
+    count ++;
+  }
+  return 'You got ' + count + ' tails in a row!';
+
+}
